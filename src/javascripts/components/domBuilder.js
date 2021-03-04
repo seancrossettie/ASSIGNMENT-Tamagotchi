567bcdea1770b29787a5130f8f0039bd7fd5f87a
Sean <jb@JBs-MacBook-Pro.local>
2021-03-04 10:24:45 -0600

    added fight buttons

--- src/javascripts/components/domBuilder.js
+++ src/javascripts/components/domBuilder.js
@@ -6,11 +6,17 @@ const domBuilder = () => {
                                                 <button id="unhealthy-food" class="btn btn-primary" type="button">Unhealthy Food</button>
                                               </div>`;
   document.querySelector('#play').innerHTML = `<h4>Eat</h4>
-                                              <h6 id='funScore' class="mt-4"></h6>
-                                              <div class="d-grid gap-2 d-md-block mt-4">
-                                                <button id="fun-activity" class="btn btn-primary" type="button">Fun Activity</button>
-                                                <button id="super-fun-activity" class="btn btn-primary" type="button">Super Fun Activity</button>
-                                              </div>`;
+                                               <h6 id='funScore' class="mt-4"></h6>
+                                               <div class="d-grid gap-2 d-md-block mt-4">
+                                                  <button id="fun-activity" class="btn btn-primary" type="button">Fun Activity</button>
+                                                  <button id="super-fun-activity" class="btn btn-primary" type="button">Super Fun Activity</button>
+                                               </div>`;
+  document.querySelector('#fight').innerHTML = `<h4>Fight</h4>
+                                                <h6 id='fightScore' class="mt-4"></h6>
+                                                <div class="d-grid gap-2 d-md-block mt-4">
+                                                  <button id="run-away" class="btn btn-primary" type="button">Run Away</button>
+                                                  <button id="commit-violence" class="btn btn-primary" type="button">Commit Violence</button>
+                                                </div>`;
 };
 
 export default domBuilder;
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,10 +3,12 @@ import domBuilder from './components/domBuilder';
 
 let full = 100;
 let fun = 50;
+let strength = 100;
 
 const printScore = () => {
   document.querySelector('#fullScore').innerHTML = full;
   document.querySelector('#funScore').innerHTML = fun;
+  document.querySelector('#fightScore').innerHTML = strength;
 };
 
 const foodEvent = (e) => {
@@ -46,11 +48,29 @@ const funEvent = (e) => {
   }
 };
 
+const fightEvent = (e) => {
+  const targetId = e.target.id;
+  if (targetId === 'run-away') {
+    if (strength + 1 < 100) {
+      strength += 1;
+      printScore();
+    } else if (strength + 1 >= 100) {
+      strength = 100;
+      printScore();
+    }
+  } else if (targetId === 'commit-violence') {
+    strength -= 10;
+    printScore();
+  }
+};
+
 const buttonEvents = () => {
   document.querySelector('#healthy-food').addEventListener('click', foodEvent);
   document.querySelector('#unhealthy-food').addEventListener('click', foodEvent);
   document.querySelector('#fun-activity').addEventListener('click', funEvent);
   document.querySelector('#super-fun-activity').addEventListener('click', funEvent);
+  document.querySelector('#run-away').addEventListener('click', fightEvent);
+  document.querySelector('#commit-violence').addEventListener('click', fightEvent);
 };
 
 domBuilder();
